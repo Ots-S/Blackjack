52ff01870eb2e4ea2450bf929cf1b2285e1d7184
jerome valette <valettejerome31@gmail.com>
2019-09-25 11:45:48 +0200

    creation carte 1 aleatoire joueur

--- Play.java
+++ Play.java
@@ -1,46 +1,45 @@
 import java.util.Random;
-import java.util.Scanner; 
+import java.util.Scanner;
+
 class Play {
-    
-    int joueur1Carte1Chiffre;
-    String joueur1Carte1Nom;
 
+    public static int joueur1Carte1Chiffre;
+    public static String joueur1Carte1Nom;
+    public static String carteJoueur1;
 
-
-    public static int getRandomIntegerBetweenRange(int min, int max){
-        int x = (int)(Math.random()*((max-min)+1))+min;
+    public static int getRandomIntegerBetweenRange(int min, int max) {
+        int x = (int) (Math.random() * ((max - min) + 1)) + min;
         return x;
     }
 
-    public static String distribution(String carteJoueur1,int valeurcarte1 ) {
-        joueur1Carte1Nom=
+    public static String distributionNomCarte() {
+   
+        String[] deckName = { "club", "diamond", "heart", "spade" };
+        int nb = getRandomIntegerBetweenRange(0, 3);
+        joueur1Carte1Nom = deckName[nb];
+        return joueur1Carte1Nom;
+    }
+    public static int distributionNumberCarte(){
+        joueur1Carte1Chiffre = getRandomIntegerBetweenRange(1, 12);
+        return joueur1Carte1Chiffre;
+    }
+    public static String carteJoueur1(){
 
-        return 0;
+        return distributionNumberCarte() + " " + distributionNomCarte();
     }
 
 
- 
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner objectI played blackjack with me
+        Scanner myObj = new Scanner(System.in); // Create a Scanner objectI played blackjack with me
         System.out.println("I played blackjack with me");
         System.out.println("Enter your name");
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Welcome: " + userName);  // Output user input 
+        String userName = myObj.nextLine(); // Read user input
+        System.out.println("Welcome: " + userName); // Output user input
 
-        System.out.println("Enter choix");
+        carteJoueur1=carteJoueur1();
+        
+        System.out.println(carteJoueur1);
 
-      }
-
-     
-
-
-
-
-
-
-
-
-
-
+    }
 
 }
