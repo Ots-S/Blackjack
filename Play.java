912ec99cbd84858d4a3cea1489811165b2bee966
Sebastien Ots <ots.sebastien@gmail.com>
2019-09-25 17:14:13 +0200

    final

--- Play.java
+++ Play.java
@@ -7,7 +7,7 @@ class Play {
     // public static String joueur1Carte1Nom;
     public static String carteJoueur1;
     public static int totalCroupier;
-
+    public static String userName;
     public static int getRandomIntegerBetweenRange(int min, int max) {
         int x = (int) (Math.random() * ((max - min) + 1)) + min;
 
@@ -58,77 +58,78 @@ class Play {
 
     public static String compare(int joueur, int croupier) {
         if (joueur > 21) {
-            return "le joueur a perdu";
+            return userName + ", You lose !";
         }
         if (croupier > 21) {
-            return "le croupier a perdu";
+            return "The dealer's total is " + totalCroupier + ", You win !";
         }
         if (joueur > croupier) {
-            return "le joueur a Gagner";
+            return userName + ", You Win !";
         }
         if (joueur == croupier) {
-            return "Egalité";
+            return "Deuce !";
         } else {
-            return "le croupier a gagné";
+            return "The dealer's total is " + totalCroupier + ", the dealer wins !";
         }
 
     }
 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in); // Create a Scanner objectI played blackjack with me
-        // System.out.println("I played blackjack with me");
-        // System.out.println("Enter your name");
-        // String userName = myObj.nextLine(); // Read user input
-        // System.out.println("Welcome: " + userName); // Output user input
+        System.out.println("Hi, Welcome to Wild Code School CASINO !");
+        System.out.println("Please enter your name :");
+        userName = myObj.nextLine(); // Read user input
+        System.out.println("Welcome " + userName + ", nice to see you !"); // Output user input
 
         int carte1Joueur = distributionNumberCarte("Joueur");
         int carte2Joueur = distributionNumberCarte("Joueur");
 
-        System.out.println("Carte 1 joueur = " + carte1Joueur);
-        System.out.println("Carte 2 joueur = " + carte2Joueur);
+        System.out.println(userName + ", Your first card is " + carte1Joueur + ".");
+        System.out.println(userName + ", Your second card is " + carte2Joueur+ ".");
 
         int total = carte1Joueur + carte2Joueur;
-        System.out.println("total Joueur = " + total);
+        System.out.println(userName + ", Your total is " + total);
 
         // CROUPIER
 
         int carte1Croupier = distributionNumberCarte("Croupier");
         int carte2Croupier = distributionNumberCarte("Croupier");
 
-        System.out.println("Carte 1 croupier = " + carte1Croupier);
-        System.out.println("Carte 2 croupier = " + "carte cachée");
+        System.out.println("The dealer's first card is " + carte1Croupier+ ".");
+        System.out.println("The dealer's second card is face down.");
 
          totalCroupier = carte1Croupier + carte2Croupier;
-        System.out.println("total visible Croupier = " + carte1Croupier);
-     
+        System.out.println("The dealer's visible total is " + carte1Croupier+ ".");
+       
         
         int carte;
 
-        while (total < 21 && totalCroupier < 21) {
+        while (total <= 21 && totalCroupier <= 21) {
         //joueur
-            System.out.println("Voulez-vous une autre carte ?");
-            System.out.println("oui ou non");
+            System.out.println("Do you want to draw another card ?");
+            System.out.println("yes or no");
             String reponse = myObj.nextLine();
 
-            if (reponse.equals("oui")) {
+            if (reponse.equals("yes")) {
                 carte = distributionNumberCarte("Joueur");
-                System.out.println("Votre carte:" + carte);
+                System.out.println(userName + ": Your card is " + carte + ".");
                 total = total + carte;
-                System.out.println("total Joueur = " + total);
+                System.out.println(userName + ": Your total is " + total + ".");
 
-            } else {
+            } else  {
                 int carteCroupier;
-                System.out.println("Le croupier retourne sa deuxième carte : " + carte2Croupier);
-                System.out.println("total Croupier = " + totalCroupier);
+                System.out.println("The dealer's face-down card is turned up, it's " + carte2Croupier + ".");
+                System.out.println("The dealer's total is " + totalCroupier + ".");
                 while (totalCroupier <= 16) {
                     carteCroupier = distributionNumberCarte("Croupier");
-                    System.out.println("nouvelle carte Croupier :" + carteCroupier);
+                    System.out.println("The dealer's new card is " + carteCroupier + ".");
                     totalCroupier = totalCroupier + carteCroupier;
+                    System.out.println("The dealer's total is " + totalCroupier + ".");
 
                 }
 
                 
-                System.out.println("total Croupier = " + totalCroupier);
+                
                 System.out.println(compare(total, totalCroupier));
                 return;
             }
