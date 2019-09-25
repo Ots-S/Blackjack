1f612fd002ba2c60c38ae011807fe0620cadb752
Sebastien Ots <ots.sebastien@gmail.com>
2019-09-25 10:23:01 +0200

    ajout main

---
+++ Play.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+class Play {
+
+
+    public static String Distribution() {
+
+        
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] deckNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        String[] deckName = {club, diamond, heart, spade};
+
+        
+
+
+
+    }
+     
+
+
+
+
+
+
+
+
+
+
+
+}
