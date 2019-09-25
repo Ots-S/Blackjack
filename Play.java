e206cbf275b32c0f62cd01d1b772147042d19da5
Sebastien Ots <ots.sebastien@gmail.com>
2019-09-25 10:47:23 +0200

    ajout random

--- Play.java
+++ Play.java
@@ -2,19 +2,28 @@ import java.util.Random;
 
 class Play {
 
+    public static int getRandomIntegerBetweenRange(int min, int max){
 
-    public static String Distribution() {
-
-        
-
-
+        int x = (int)(Math.random()*((max-min)+1))+min;
+    
+        return x;
+    
     }
+
+    public static String distribution() {
+
+
+}
+
+
+  
 
     public static void main(String[] args) {
 
         int[] deckNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        String[] deckName = {club, diamond, heart, spade};
-
+        String[] deckName = {"club", "diamond", "heart", "spade"};
+        int x = getRandomIntegerBetweenRange(deckNumber[0], deckNumber[11]);
+        System.out.println(x);
         
 
 
