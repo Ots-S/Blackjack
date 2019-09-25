ed5297293bb8c832bef4189fe68488c466abab09
jerome valette <valettejerome31@gmail.com>
2019-09-25 10:51:23 +0200

    first

--- Play.java
+++ Play.java
@@ -7,19 +7,21 @@ class Play {
 
         
 
-
+        return 0;
     }
 
+ 
     public static void main(String[] args) {
 
         int[] deckNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        String[] deckName = {club, diamond, heart, spade};
-
-        
-
-
-
+        String[] deckName = {"club", "diamond", "heart","spade"};
+        System.out.println(myRandomInteger(1,10));
     }
+
+      public static  myRandomInteger(int min, int max)
+        {
+            return (int) (min + Math.random() * (max - min + 1));
+        }
      
 
 
